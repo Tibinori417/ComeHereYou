780663cfd65b15b2f5989b4128acc779d916df4a
Tibinori417 <417ringo4649@gmail.com>
2024-06-28 18:13:21 +0900

    first commit

---
+++ src/renderer/webgl/shaders/index.js
@@ -0,0 +1,48 @@
+/**
+ * @author       Richard Davey <rich@phaser.io>
+ * @copyright    2013-2024 Phaser Studio Inc.
+ * @license      {@link https://opensource.org/licenses/MIT|MIT License}
+ */
+
+/**
+ * @namespace Phaser.Renderer.WebGL.Shaders
+ */
+
+module.exports = {
+
+    AddBlendFrag: require('./AddBlend-frag'),
+    BitmapMaskFrag: require('./BitmapMask-frag'),
+    BitmapMaskVert: require('./BitmapMask-vert'),
+    ColorMatrixFrag: require('./ColorMatrix-frag'),
+    CopyFrag: require('./Copy-frag'),
+    FXBarrelFrag: require('./FXBarrel-frag'),
+    FXBloomFrag: require('./FXBloom-frag'),
+    FXBlurHighFrag: require('./FXBlurHigh-frag'),
+    FXBlurLowFrag: require('./FXBlurLow-frag'),
+    FXBlurMedFrag: require('./FXBlurMed-frag'),
+    FXBokehFrag: require('./FXBokeh-frag'),
+    FXCircleFrag: require('./FXCircle-frag'),
+    FXDisplacementFrag: require('./FXDisplacement-frag'),
+    FXGlowFrag: require('./FXGlow-frag'),
+    FXGradientFrag: require('./FXGradient-frag'),
+    FXPixelateFrag: require('./FXPixelate-frag'),
+    FXShadowFrag: require('./FXShadow-frag'),
+    FXShineFrag: require('./FXShine-frag'),
+    FXVignetteFrag: require('./FXVignette-frag'),
+    FXWipeFrag: require('./FXWipe-frag'),
+    LightFrag: require('./Light-frag'),
+    LinearBlendFrag: require('./LinearBlend-frag'),
+    MeshFrag: require('./Mesh-frag'),
+    MeshVert: require('./Mesh-vert'),
+    MobileFrag: require('./Mobile-frag'),
+    MobileVert: require('./Mobile-vert'),
+    MultiFrag: require('./Multi-frag'),
+    MultiVert: require('./Multi-vert'),
+    PointLightFrag: require('./PointLight-frag'),
+    PointLightVert: require('./PointLight-vert'),
+    PostFXFrag: require('./PostFX-frag'),
+    QuadVert: require('./Quad-vert'),
+    SingleFrag: require('./Single-frag'),
+    SingleVert: require('./Single-vert')
+
+};
